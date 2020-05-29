44d53a5c64200720793a992b2094653d574c9ced
Nick Scadden <nds37@uclive.ac.nz>
2020-05-29 19:57:48 +1200

    Switch header file

---
+++ Lab3/switch.h
@@ -0,0 +1,17 @@
+/* FitnessTue9Group7
+ *
+ * switch.h
+ *
+ *  Created on: 23/05/2020
+ *      Author: Sean & Nick
+ */
+
+#ifndef SWITCH_H_
+#define SWITCH_H_
+
+void
+initSwitch (void);
+
+
+
+#endif /* SWITCH_H_ */
